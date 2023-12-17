d1a1c896dd99816c589ab32fab8acce9be37b051
shahzaib irfan <shahzaibirfan1012@gmail.com>
2023-12-17 13:35:03 +0500

    Add Room Complete

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,26 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from  './components/navbar';
-import {Route, BrowserRouter , Routes,  Link} from 'react-router-dom';
-import Homescreen from './screens/Homescreen';
-import Bookingscreen from './screens/Bookingscreen';
-import Loginscreen from './screens/Loginscreen';
-import Registerscreen from './screens/Registerscreen';
-import Profilescreen from './screens/Profilescreen';
-import Adminscreen from './screens/Adminscreen';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar";
+import { Route, BrowserRouter, Routes, Link } from "react-router-dom";
+import Homescreen from "./screens/Homescreen";
+import Bookingscreen from "./screens/Bookingscreen";
+import Loginscreen from "./screens/Loginscreen";
+import Registerscreen from "./screens/Registerscreen";
+import Profilescreen from "./screens/Profilescreen";
+import Adminscreen from "./screens/Adminscreen";
+import AdminRooms from "./components/AdminRooms";
+import AddRoom from "./components/AddRoom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <BrowserRouter>
         <Routes>
           <Route path="/home" exact Component={Homescreen} />
-          <Route path="/book/:roomid/:fromdate/:todate" exact Component={Bookingscreen} />
+          <Route path="/managerooms" element={<AdminRooms />} />
+          <Route path="/managerooms/addroom" element={<AddRoom />} />
+          <Route
+            path="/book/:roomid/:fromdate/:todate"
+            exact
+            Component={Bookingscreen}
+          />
           <Route path="/Login" exact Component={Loginscreen} />
           <Route path="/Register" exact Component={Registerscreen} />
           <Route path="/profile" exact Component={Profilescreen} />
-          <Route path="/Admin" exact Component={Adminscreen} /> 
+          <Route path="/Admin" exact Component={Adminscreen} />
         </Routes>
       </BrowserRouter>
     </div>
--- my-react-app/src/components/navbar.js
+++ my-react-app/src/components/navbar.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
   // Retrieve user information from localStorage
-  const User = JSON.parse(localStorage.getItem('currentUser'));
-  function LogOut()
-  {
+  const User = JSON.parse(localStorage.getItem("currentUser"));
+  function LogOut() {
     localStorage.removeItem("currentUser");
     window.location.href = "/login";
   }
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: 'black' }}>
+    <nav
+      className="navbar navbar-expand-lg navbar-dark"
+      style={{ backgroundColor: "black" }}
+    >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/home" style={{ color: 'white' }}>
-          Second Monal
+        <a className="navbar-brand" href="/home" style={{ color: "white" }}>
+          The Bliss Adventures
         </a>
         <button
           className="navbar-toggler"
@@ -23,35 +25,59 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon" ><i class="fa-solid fa-bars"></i></span>
+          <span className="navbar-toggler-icon">
+            <i class="fa-solid fa-bars"></i>
+          </span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
-              {User && User.user && User.user.name ? (
-                    <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" style={{ backgroundColor: 'black' }}>
-                    <i class="fa-solid fa-user">{User.user.name}</i>
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                      <li><a class="dropdown-item" href="/profile">Profile</a></li>
-                      <li><a class="dropdown-item" href="#" onClick={LogOut}>Log Out</a></li>
-                    </ul>
-                  </div>
-                ) : (
-                  <>
-                    <li className="nav-item">
-                      <a className="nav-link" href="http://localhost:3000/Register" style={{ color: 'white' }}>
-                        Register
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="http://localhost:3000/Login" style={{ color: 'white' }}>
-                        Login
-                      </a>
-                    </li>
-                  </>
-                )}
-
+            {User && User.user && User.user.name ? (
+              <div class="dropdown">
+                <button
+                  class="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  style={{ backgroundColor: "black" }}
+                >
+                  <i class="fa-solid fa-user">{User.user.name}</i>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                  <li>
+                    <a class="dropdown-item" href="/profile">
+                      Profile
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#" onClick={LogOut}>
+                      Log Out
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    href="http://localhost:3000/Register"
+                    style={{ color: "white" }}
+                  >
+                    Register
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    href="http://localhost:3000/Login"
+                    style={{ color: "white" }}
+                  >
+                    Login
+                  </a>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
