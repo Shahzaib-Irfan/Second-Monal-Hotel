4d370f6190dc7cc8ce8815bd092f56fe6b22b70f
buttafraz22 <109384539+buttafraz22@users.noreply.github.com>
2023-12-19 00:00:24 +0500

    Add ReadME and close #11

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -14,7 +14,7 @@ import BookRoom from "./components/BookRoom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <Navbar />
       <BrowserRouter>
         <Routes>
